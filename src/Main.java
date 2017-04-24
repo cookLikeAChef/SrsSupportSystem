c142c20e10dfc4671a7c0adff2469c035307deda
Kwonjae Lee <kwonjae13@naver.com>
2017-04-24 18:15:23 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by kwonj on 2017-04-24.
+ */
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("상단에 표시되는 글자");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
